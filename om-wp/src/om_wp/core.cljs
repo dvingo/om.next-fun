4dbc6b3ed1b21c22fa6be827208e39f42bddd068
Dan Vingo <danvingo@gmail.com>
2017-03-13 21:59:57 -0400

    First component

--- om-wp/src/om_wp/core.cljs
+++ om-wp/src/om_wp/core.cljs
@@ -1,12 +1,18 @@
 (ns om-wp.core
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require-macros [cljs.core.async.macros :refer [go-loop]])
   (:require
     [sablono.core :as sabl :refer-macros [html]]
-    [cljs.core.async :as async :refer [<! >! put! chan]]
+    [cljs.core.async :as async :refer [<! put! chan]]
     [cljs.core.match :refer-macros [match]]
     [om.next :as om :refer-macros [defui]]
     [om.dom :as dom :include-macros true]
     [clojure.string :as string]))
 
 (enable-console-print!)
-(println "Testing")
+
+(defui App
+  Object
+  (render [this]
+    (html [:div "First component"])))
+
+(om/add-root! (om/reconciler {}) App js/app)
